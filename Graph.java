fce5730b13ada43311976fa0442cf97e12be85c4
tammurmas <tammurmas@gmail.com>
2013-11-02 13:43:00 +0200

    esimene versioon
    esimene versioon

---
+++ Graph.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tamm.aa.hw9;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dell
+ */
+public class Graph {
+    
+    public static void main(String[] args)
+    {
+        ArrayList<String> adjList = new ArrayList<String>();
+    }
+    
+}
